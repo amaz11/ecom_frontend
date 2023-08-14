68bc9a030a9d6e87bd95f13f115cc0b7a7ea9a67
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2023-08-14 21:19:11 +0600

    login resgiter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { routers } from "./routes/routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <RouterProvider router={routers} />
     </>
   );
--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,5 +1,174 @@
-const Authentication = () => {
-  return <div>Authentication</div>;
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import logo from "../assets/seven2-logo-15153843922.jpg";
+import person from "../assets/personProfile.png";
+
+import { Link } from "react-router-dom";
+const Authentication: React.FC = () => {
+  const [passShow, setPassShow] = useState<boolean>(false);
+  const isLogin = window.location.pathname === "/authentication/login";
+  let i, j;
+  for (i = 5; i >= 1; i--) {
+    for (j = 1; j <= i; j++) {
+      console.log("*");
+    }
+  }
+  return (
+    <>
+      <div className="flex flex-col justify-center min-h-screen py-6 mx-3 bg-gray-100 sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 transform -skew-y-6 shadow-lg bg-gradient-to-r from-slate-200 to-orange-500 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="max-w-md">
+              <div className="flex justify-center mx-auto">
+                <img src={logo} alt="" />
+              </div>
+              <form action="">
+                <div className="divide-y divide-gray-200">
+                  <div className="py-8 space-y-4 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7">
+                    {isLogin ? null : (
+                      <>
+                        <div
+                          className={`w-[150px] h-[150px] border-2 border-orange-500 rounded-full mx-auto  bg-bottom bg-cover relative`}
+                          style={{
+                            backgroundImage: `url(${person})`,
+                            backgroundRepeat: "no-repeat",
+                            objectFit: "cover",
+                            backgroundPosition: "0px 5px",
+                          }}
+                        >
+                          <input
+                            type="file"
+                            className="absolute top-0 left-0 z-20 w-full h-full rounded-full opacity-0 cursor-pointer"
+                            name=""
+                            id=""
+                          />
+                          <div className="absolute bottom-0 right-0 z-10 w-12 h-12 p-3 overflow-hidden bg-white border border-orange-500 rounded-full cursor-pointer">
+                            <img
+                              className="w-full"
+                              src="https://www.citypng.com/public/uploads/preview/hd-camera-orange-icon-png-116395931094fajokdwqu.png"
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                        <div className="relative">
+                          <input
+                            autoComplete="off"
+                            name="name"
+                            type="text"
+                            className="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-gray-300 peer focus:outline-none focus:borer-rose-600"
+                            placeholder="Email address"
+                          />
+                          <label
+                            htmlFor="email"
+                            className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                          >
+                            Your Name
+                          </label>
+                        </div>
+                      </>
+                    )}
+                    <div className="relative">
+                      <input
+                        autoComplete="off"
+                        name="email"
+                        type="email"
+                        className="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-gray-300 peer focus:outline-none focus:borer-rose-600"
+                        placeholder="Email address"
+                      />
+                      <label
+                        htmlFor="email"
+                        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                      >
+                        Email Address
+                      </label>
+                    </div>
+                    <div className="relative">
+                      <input
+                        autoComplete="off"
+                        name="password"
+                        type={`${passShow ? "text" : "password"}`}
+                        className="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-gray-300 peer focus:outline-none focus:borer-rose-600"
+                        placeholder="Password"
+                      />
+                      <label
+                        htmlFor="password"
+                        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                      >
+                        Password
+                      </label>
+                      {passShow ? (
+                        <AiOutlineEyeInvisible
+                          onClick={() => setPassShow(!passShow)}
+                          className="absolute right-0 top-4"
+                        />
+                      ) : (
+                        <AiOutlineEye
+                          onClick={() => setPassShow(!passShow)}
+                          className="absolute right-0 top-4"
+                        />
+                      )}
+                    </div>
+                    {isLogin ? null : (
+                      <div className="relative">
+                        <input
+                          autoComplete="off"
+                          name="conpassword"
+                          type={`${passShow ? "text" : "password"}`}
+                          className="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-gray-300 peer focus:outline-none focus:borer-rose-600"
+                          placeholder="Confirm Password"
+                        />
+                        <label
+                          htmlFor="password"
+                          className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                        >
+                          Confirm Password
+                        </label>
+                        {passShow ? (
+                          <AiOutlineEyeInvisible
+                            onClick={() => setPassShow(!passShow)}
+                            className="absolute right-0 top-4"
+                          />
+                        ) : (
+                          <AiOutlineEye
+                            onClick={() => setPassShow(!passShow)}
+                            className="absolute right-0 top-4"
+                          />
+                        )}
+                      </div>
+                    )}
+
+                    <div className="relative">
+                      <button className="px-3 py-0.5 font-semibold text-white bg-orange-500 hover:bg-black">
+                        Register Now!
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div>
+              {isLogin ? (
+                <Link
+                  to="/authentication/registration"
+                  className="hover:text-orange-500"
+                >
+                  Don't have a account? Click here!
+                </Link>
+              ) : (
+                <Link
+                  to="/authentication/login"
+                  className="hover:text-orange-500"
+                >
+                  Already Have a account! Click here.
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Authentication;
